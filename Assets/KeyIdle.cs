9ad0630165d4a0e225d0a8ded6336eca5a5fb014
Ammar <ammarmz@hotmail.com>
2019-09-19 21:28:39 -0400

    Added keys and idle key script to maze

---
+++ Assets/KeyIdle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyIdle : MonoBehaviour
+{
+    private float rotationSpeed = 1.0f;
+    private float rotationY = 90.0f;
+    private float height = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY -= 40.0f * Time.deltaTime;
+        Quaternion newRotation = Quaternion.Euler (0, rotationY, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime);
+
+    }
+}
